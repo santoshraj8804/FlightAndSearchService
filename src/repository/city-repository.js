bfd705746161af2b9475d6818d07cf4de49175d2
Santosh raj <santoshrajgupta7366@gmail.com>
2025-05-31 16:51:17 +0530

    Added city service

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,6 +1,6 @@
 const { City } = require("../models/index");
 
-class CityRepository {
+class CityRepository{
     async createCity({ name }) {
         try {
              const city = await City.create({ name });
@@ -42,4 +42,4 @@ class CityRepository {
     
 }
 
-module.exports = CityRepository();
+module.exports = CityRepository;
