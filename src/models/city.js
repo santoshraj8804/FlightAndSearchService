94fb442cf5989ef6c483b7f9d16b8135af79a10b
Santosh raj <santoshrajgupta7366@gmail.com>
2025-06-02 17:35:39 +0530

    Added filters to get all cities

--- src/models/city.js
+++ src/models/city.js
@@ -1,3 +1,29 @@
+// 'use strict';
+// const {
+//   Model
+// } = require('sequelize');
+// module.exports = (sequelize, DataTypes) => {
+//   class City extends Model {
+//     /**
+//      * Helper method for defining associations.
+//      * This method is not a part of Sequelize lifecycle.
+//      * The `models/index` file will call this method automatically.
+//      */
+//     static associate(models) {
+//       // define association here
+//     }
+//   }
+//   City.init({
+//     name: DataTypes.STRING,
+//     allowNull: false,
+//     unique: true
+//   }, {
+//     sequelize,
+//     modelName: 'City',
+//   });
+//   return City;
+// };
+
 'use strict';
 const {
   Model
@@ -7,17 +33,18 @@ module.exports = (sequelize, DataTypes) => {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
+     * The models/index file will call this method automatically.
      */
     static associate(models) {
       // define association here
     }
   }
   City.init({
-    name: DataTypes.STRING,
-    allowNull: false,
-    unique: true
-  }, {
+    name: {
+      type: DataTypes.STRING,
+      allowNull:false,
+      unique:true
+    }  }, {
     sequelize,
     modelName: 'City',
   });
--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,3 +1,5 @@
+const { Op } = require("sequelize"); 
+
 const { City } = require("../models/index");
 
 class CityRepository{
@@ -40,11 +42,22 @@ class CityRepository{
         }
     } 
 
-    async getAllCities() {
-         try {
-             const cities = await City.findAll({
-                attributes: ['id', 'name', 'createdAt', 'updatedAt'] // ✅ Only real DB fields
-            });
+    async getAllCities(filter) {
+          try {
+               if(filter.name) {
+                    const cities = await City.findAll({
+                         where: {
+                              name: {
+                                   [Op.startsWith]: filter.name
+                              }
+                         }
+                    });
+                    return cities;
+               }
+          //    const cities = await City.findAll({
+          //       attributes: ['id', 'name', 'createdAt', 'updatedAt'] // ✅ Only real DB fields);
+          //    });
+               const cities = await City.findAll();
              return cities;
         } catch (error) {
              console.log("Something went wrong in the repository layer"); 
