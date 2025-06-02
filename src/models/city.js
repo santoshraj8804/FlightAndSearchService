115ae28f3a9c8c7ba11df842d2125e8c76dc8bc3
Santosh raj <santoshrajgupta7366@gmail.com>
2025-06-02 18:20:14 +0530

    Added airports model

--- src/models/city.js
+++ src/models/city.js
@@ -37,6 +37,9 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      this.hasMany(models.Airport,{
+        foreignKey:"cityId"
+      });
     }
   }
   City.init({
