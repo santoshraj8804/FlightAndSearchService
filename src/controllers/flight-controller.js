99bc890796358f14d96cf9369886678a8c715d7c
Santosh raj <santoshrajgupta7366@gmail.com>
2025-06-03 16:50:10 +0530

    Added flights create crud

---
+++ src/controllers/flight-controller.js
@@ -0,0 +1,27 @@
+const {FlightService} = require("../services/index");
+
+const flightService = new FlightService();
+
+const create = async (req, res) => {
+    try {
+        const flight = await flightService.createFlight(req.body);
+        return res.status(201).json({
+            data: flight,
+            success: true,
+            err: {},
+            message: "Flight created successfully"
+        });
+    } catch (error) {
+        console.log("error");
+        return res.status(500).json({
+            data: {},
+            success: false,
+            err: error,
+            message: "Not able to create Flight"
+        });
+    }
+}
+
+module.exports = {
+    create
+}
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const CityController = require('../../controllers/city-controller');
+const FlightController = require('../../controllers/flight-controller');
+
 const router = express.Router();
 
 router.post('/city', CityController.create);
@@ -7,5 +9,7 @@ router.delete('/city/:id', CityController.destroy);
 router.patch('/city/:id', CityController.update);
 router.get('/city', CityController.getAll);
 router.get('/city/:id', CityController.get);
+
+router.post('/flights', FlightController.create);
 
 module.exports = router;
