7f2c68fe1d2a9fa420ea038667d4e0377b3b8d21
Santosh raj <santoshrajgupta7366@gmail.com>
2025-06-10 21:10:08 +0530

    Completed flight update api

--- src/controllers/flight-controller.js
+++ src/controllers/flight-controller.js
@@ -51,10 +51,53 @@ const getAll = async (req, res) => {
         });
     }
 }
+const get = async (req, res) => {
+    try {
+        const response = await flightService.getFlight(req.params.id);
+        return res.status(SuccessCodes.OK).json({
+            data: response,
+            success: true,
+            err: {},
+            message: "Successfully fetched the Flight"
+        });
+    } catch (error) {
+        console.log("error");
+        return res.status(500).json({
+            data: {},
+            success: false,
+            err: error,
+            message: "Not able to fetch the Flight"
+        });
+    }
+}
+
+const update = async (req, res) => {
+    try {
+        const response = await flightService.updateFlight(req.params.id, req.body);
+        return res.status(SuccessCodes.OK).json({
+            data: response,
+            success: true,
+            err: {},
+            message: "Successfully updated the Flight"
+        });
+    } catch (error) {
+        console.log("error");
+        return res.status(500).json({
+            data: {},
+            success: false,
+            err: error,
+            message: "Not able to update the Flight"
+        });
+    }
+}
+
+
 
 
 
 module.exports = {
     create,
-    getAll
+    getAll,
+    get,
+    update
 };
